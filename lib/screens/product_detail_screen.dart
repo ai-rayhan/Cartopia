72dc846aa5816f99bc2e585ab23ca9d06752c722
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-03-10 11:02:40 +0600

    product detail update

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,17 +10,32 @@ class ProductDetailScreen extends StatelessWidget {
   // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
+  const ProductDetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context)?.settings.arguments as String; // is the id!
     print(productId);
-    final loadedProducts = Provider.of<Products>(context).findById(productId);
+    final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(loadedProducts.title),
+        title: Text(loadedProduct.title),
       ),
-      body: Center(child: Image.asset(loadedProducts.imageUrl)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.all(40.0),
+                height: 300,
+                width: double.infinity,
+                child: Image.asset(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.fitHeight,
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
